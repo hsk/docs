ee2e6fe7d6ce3b3ab0c33c1c4a97bdb2e8c75529
Hiroshi Sakurai <sakurai777@gmail.com>
2014-08-17 17:32:35 +0900

    added typetheory epub files.

---
+++ typetheory/mkepub.sh
@@ -0,0 +1,12 @@
+pandoc title.txt \
+  README.md \
+  set.md \
+  set01.md \
+  set02.md \
+  set03.md \
+  set04.md \
+  set05.md \
+  set99.md \
+  -o typetheory.epub \
+  --epub-stylesheet=css/epub.css --smart
+open typetheory.epub
