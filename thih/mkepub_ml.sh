58982aed5138f2b0e946faa9aa1424a3719c7a72
Hiroshi Sakurai <sakurai777@gmail.com>
2014-08-22 11:21:57 +0900

    added ml epub files.

---
+++ thih/mkepub_ml.sh
@@ -0,0 +1,15 @@
+pandoc title_ml.txt \
+  chap01/README_ml.md \
+  chap02/README_ml.md \
+  chap03/README_ml.md \
+  chap04/README_ml.md \
+  chap05/README_ml.md \
+  chap06/README_ml.md \
+  chap07/README_ml.md \
+  chap08/README_ml.md \
+  chap09/README_ml.md \
+  chap10/README_ml.md \
+  chap11/README_ml.md \
+  -o thih_ml.epub \
+  --epub-stylesheet=epub.css --smart
+open thih_ml.epub
