f99ca4b0650222446d2a619d05537a58f2ef272b
Hiroshi Sakurai <sakurai777@gmail.com>
2014-09-17 18:45:51 +0900

    added scheme tests.

--- thih/scala/src/test/scala/SchemeSpec.scala
+++ thih/scala/src/test/scala/SchemeSpec.scala
@@ -4,15 +4,51 @@ import org.scalatest.Matchers._
 import thih._
 
 class SchemeSpec extends FlatSpec {
-  it should "test" in {
-    "a" shouldBe "a"
+
+  import Type._
+  import Kind._
+  import Scheme._
+  import Pred._
+
+  it should "scheme" in {
+
+    val ty = TVar(Tyvar("a", Star))
+    val pred = IsIn("Num", ty)
+    val sc = Forall(List(),Qual(List(pred),ty))
+    printf("scheme = %s\n", sc)
   }
 
-  it should "test1" in {
-    "a" shouldBe "a"
+  it should "schemeApply" in {
+    val ty = TVar(Tyvar("a", Star))
+    val pred = IsIn("Num", ty)
+    val sc = Forall(List(),Qual(List(pred),ty))
+    val subst = List((Tyvar("a", Star), tInt))
+    val sc1 = schemeApply(subst)(sc)
+    printf("scheme = %s\n", sc1)
+
   }
 
-  it should "test2" in {
-    "a" shouldBe "a"
+  it should "schemeTv" in {
+    val ty = TVar(Tyvar("a", Star))
+    val pred = IsIn("Num", ty)
+    val sc = Forall(List(),Qual(List(pred),ty))
+    val tvs = schemeTv(sc)
+    printf("tvs = %s\n", tvs)
+  }
+
+  it should "quantify" in {
+    val tyvar = Tyvar("a", Star)
+    val ty = TVar(Tyvar("a", Star))
+    val pred = IsIn("Num", ty)
+    val qual = Qual(List(pred), fn(ty)(tInt))
+    val sc = quantify(List(tyvar))(qual)
+    printf("scheme = %s\n", sc)
+
+  }
+
+  it should "toScheme" in {
+    val ty = TVar(Tyvar("a", Star))
+    val sc = toScheme(ty)
+    printf("scheme = %s\n", sc)
   }
 }
