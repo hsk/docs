8da84c2ad596e2f1567c4a35f3f3235000863878
Hiroshi Sakurai <sakurai777@gmail.com>
2014-09-30 00:16:02 +0900

    added haddock comments.

--- haskell/07doctest/a.hs
+++ haskell/07doctest/a.hs
@@ -1,6 +1,18 @@
+{- |
+Module      : A
+Description : ドキュメンテーションテスト
+Copyright   : (c) h_sakurai, 2014
+License     : free-BSD
+Maintainer  : test@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+ここにモジュールの詳細な記述を書く．
+-}
+
 module A where
 -- |
--- Id.
+-- 渡された値をそのまま返します。
 --
 -- >>> id1 "a"
 -- "a"
