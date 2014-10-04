737dea54e8190cf6aa9e0a12a72171a548e17618
Hiroshi Sakurai <sakurai777@gmail.com>
2014-10-04 10:02:34 +0900

    added trait 2d

--- traittable/fib/README.md
+++ traittable/fib/README.md
@@ -1,14 +1,14 @@
 # Fib 40 bench
 
-| language | fib | object | trait | trait opt |
+| language | fib | object | trait | trait 2d | trait opt |
 | --- | ---- | ---- | --- | ---- |
-| g++ -O3 | 474ms | 1,031ms | 1,220ms | 926ms |
-| Java | 627ms | 1,110ms | 1,576ms |     |
-| Node | 2,085ms | 3,754ms |   |   |
-| Ruby | 23,380ms | 121,465ms | | |
-| Rust --opt-level=3| 548ms | 609ms | | |
-| Rust -O| 550ms | 1,032ms | | |
-| Rust | 1,355ms | 3,927ms | | |
-| ocamlopt | 771ms | 13,791ms | | |
-| ocamlc | 6,516ms | 49.043ms | | |
-| gcc fib.m -O3 -fobjc-arc| 474ms | 151,031ms | | |
+| g++ -O3 | 474ms | 1,031ms | 1,220ms | 1399ms | 926ms |
+| Java | 627ms | 1,110ms | 1,576ms |     | |
+| Node | 2,085ms | 3,754ms |   |   | |
+| Ruby | 23,380ms | 121,465ms | | | |
+| Rust --opt-level=3| 548ms | 609ms | | | |
+| Rust -O| 550ms | 1,032ms | | | |
+| Rust | 1,355ms | 3,927ms | | | |
+| ocamlopt | 771ms | 13,791ms | | | |
+| ocamlc | 6,516ms | 49.043ms | | | |
+| gcc fib.m -O3 -fobjc-arc| 474ms | 151,031ms | | | |
--- traittable/fib/fib.cpp
+++ traittable/fib/fib.cpp
@@ -22,102 +22,229 @@ struct Fib {
 	}
 };
 
+namespace trait1 {
 
-struct Vec {
-  int size;
-  void** data;
-};
 
-Vec* newVec() {
-  Vec* v = new Vec();
-  v->size = 0;
-  v->data = new void*[v->size];
-  return v;
-}
-void setVec(Vec* v, int idx, void* d) {
-  if (idx > v->size) {
-    // resize
-    void** data = new void*[idx+1];
-    memcpy(v->data, data, sizeof(void*)*v->size);
-    v->size = idx+1;
-    delete[] v->data;
-    v->data = data;
+  struct Vec {
+    int size;
+    void** data;
+  };
+
+  Vec* newVec() {
+    Vec* v = new Vec();
+    v->size = 0;
+    v->data = new void*[v->size];
+    return v;
   }
-  v->data[idx] = d;
-}
+  void setVec(Vec* v, int idx, void* d) {
+    if (idx > v->size) {
+      // resize
+      void** data = new void*[idx+1];
+      memcpy(v->data, data, sizeof(void*)*v->size);
+      v->size = idx+1;
+      delete[] v->data;
+      v->data = data;
+    }
+    v->data[idx] = d;
+  }
 
-struct Class {
-  int id;
+  struct Class {
+    int id;
+  };
+
+  int Class_genId() {
+    static int classId = -1;
+    classId++;
+    return classId;
+  }
+
+  // struct Int
+  int Int_classId = Class_genId();
+  struct Int {
+    int id;
+    int x;
+    Int(int x):id(Int_classId),x(x){}
+  };
+
+  struct Fib2 {
+    int (*fib)(Class*);
+  };
+  Vec* Fib2_v = newVec();
+
+  int Fib2_Int_fib(Class* self) {
+    Int* p = (Int*)self;
+    if(p-> x < 2) return 1;
+
+    Int p1(p->x - 2);
+    Int p2(p->x - 1);
+
+    return ((Fib2*)Fib2_v->data[p->id])->fib((Class*)&p1) +
+        ((Fib2*)Fib2_v->data[p->id])->fib((Class*)&p2);
+  }
+
+  Fib2* newFib2_Int() {
+    Fib2 *impl = new Fib2();
+    setVec(Fib2_v, Int_classId, (void*)impl);
+    impl->fib = &Fib2_Int_fib;
+    return impl;
+  }
+  Fib2* Fib2_Int_ = newFib2_Int();
+
+  int Int3_classId = Class_genId();
+  struct Int3 {
+    int id;
+    int x;
+  };
+
+  struct Fib3 {
+    int (*fib)(Class*);
+  };
+  Vec* Fib3_v = newVec();
+
+  int Fib3_Int3_fib(Class* self) {
+    Int3* p = (Int3*)self;
+    if(p-> x < 2) return 1;
+
+    Int3 p1 ={Int3_classId, p->x - 2};
+    Int3 p2 ={Int3_classId, p->x - 1};
+
+    return Fib3_Int3_fib((Class*)&p1) + Fib3_Int3_fib((Class*)&p2);
+  }
+
+  Fib3* newFib3_Int3() {
+    Fib3 *impl = new Fib3();
+    setVec(Fib3_v, Int3_classId, (void*)impl);
+    impl->fib = &Fib3_Int3_fib;
+    return impl;
+  }
+  Fib3* Fib3_Int3_ = newFib3_Int3();
+
+  void bench() {
+    long start;
+    start = gett();
+    Int p(40);
+    printf("%d\n", ((Fib2*)Fib2_v->data[p.id])->fib((Class*)&p));
+    printf("%ld\n", gett() - start);
+
+    start = gett();
+    Int3 p3 = {Int3_classId, 40};
+    printf("%d\n", ((Fib2*)Fib3_v->data[p3.id])->fib((Class*)&p3));
+    printf("%ld\n", gett() - start);
+  }
 };
 
-int Class_genId() {
-  static int classId = -1;
-  classId++;
-  return classId;
-}
+namespace trait2 {
 
-// struct Int
-int Int_classId = Class_genId();
-struct Int {
-  int id;
-  int x;
-  Int(int x):id(Int_classId),x(x){}
+  enum {PAGE_INIT_SIZE=0,PAGE_SIZE=0x100,PAGE_BIT=8,PAGE_MASK=0xff};
+
+  struct Vec {
+    int size;
+    void*** data;
+  };
+
+  Vec* newVec() {
+    Vec* v = new Vec();
+    v->size = PAGE_INIT_SIZE;
+    v->data = new void**[v->size];
+    memset(v->data, 0, sizeof(void**)*v->size);
+    return v;
+  }
+  void setVec(Vec* v, int idx, void* d) {
+    int page = idx >> PAGE_BIT;
+    idx = idx & PAGE_MASK;
+    if (v->size <= page) {
+      printf("realloc\n");
+      void*** data = new void**[page+1];
+      memcpy(v->data, data, sizeof(void**)*v->size);
+      memset(&data[v->size], 0, sizeof(void**)*(page+1 - v->size));
+      v->size = page+1;
+      delete[] v->data;
+      v->data = data;
+    }
+    if (v->data[page] == 0) {
+      printf("page gen\n");
+      v->data[page] = new void*[PAGE_SIZE];
+    }
+    v->data[page][idx] = d;
+  }
+
+  struct Class {
+    int id;
+  };
+
+  int Class_genId() {
+    static int classId = -1;
+    classId++;
+    return classId;
+  }
+
+  // struct Int
+  int Int_classId = Class_genId();
+  struct Int {
+    int id;
+    int x;
+    inline Int(int x):id(Int_classId),x(x){}
+  };
+
+  struct Fib2 {
+    int (*fib)(Class*);
+  };
+  Vec* Fib2_v = newVec();
+
+  int Fib2_Int_fib(Class* self) {
+    Int* p = (Int*)self;
+    if(p-> x < 2) return 1;
+
+    Int p1(p->x - 2);
+    Int p2(p->x - 1);
+
+    return ((Fib2*)Fib2_v->data[p->id >> PAGE_BIT][p->id & PAGE_MASK])->fib((Class*)&p1) +
+        ((Fib2*)Fib2_v->data[p->id >> PAGE_BIT][p->id & PAGE_MASK])->fib((Class*)&p2);
+  }
+
+  Fib2* newFib2_Int() {
+    Fib2 *impl = new Fib2();
+    setVec(Fib2_v, Int_classId, (void*)impl);
+    impl->fib = &Fib2_Int_fib;
+    return impl;
+  }
+  Fib2* Fib2_Int_ = newFib2_Int();
+
+  struct Fib3 {
+    int (*fib)(Class*);
+  };
+  Vec* Fib3_v = newVec();
+
+  int Fib3_Int_fib(Class* self) {
+    Int* p = (Int*)self;
+    if(p-> x < 2) return 1;
+
+    Int p1(p->x - 2);
+    Int p2(p->x - 1);
+
+    return Fib3_Int_fib((Class*)&p1) + Fib3_Int_fib((Class*)&p2);
+  }
+
+  Fib3* newFib3_Int() {
+    Fib3 *impl = new Fib3();
+    setVec(Fib3_v, Int_classId, (void*)impl);
+    impl->fib = &Fib3_Int_fib;
+    return impl;
+  }
+  Fib3* Fib3_Int_ = newFib3_Int();
+
+  void bench() {
+    long start;
+    start = gett();
+    Int p(40);
+    printf("%d\n", ((Fib2*)Fib2_v->data[p.id >> PAGE_BIT][p.id & PAGE_MASK])->fib((Class*)&p));
+    printf("%ld\n", gett() - start);
+
+    start = gett();
+    printf("%d\n", ((Fib3*)Fib3_v->data[p.id >> PAGE_BIT][p.id & PAGE_MASK])->fib((Class*)&p));
+    printf("%ld\n", gett() - start);
+  }
 };
-
-struct Fib2 {
-  int (*fib)(Class*);
-};
-Vec* Fib2_v = newVec();
-
-int Fib2_Int_fib(Class* self) {
-  Int* p = (Int*)self;
-  if(p-> x < 2) return 1;
-
-  Int p1(p->x - 2);
-  Int p2(p->x - 1);
-
-  return ((Fib2*)Fib2_v->data[p->id])->fib((Class*)&p1) +
-      ((Fib2*)Fib2_v->data[p->id])->fib((Class*)&p2);
-}
-
-Fib2* newFib2_Int() {
-  Fib2 *impl = new Fib2();
-  setVec(Fib2_v, Int_classId, (void*)impl);
-  impl->fib = &Fib2_Int_fib;
-  return impl;
-}
-Fib2* Fib2_Int_ = newFib2_Int();
-
-int Int3_classId = Class_genId();
-struct Int3 {
-  int id;
-  int x;
-};
-
-struct Fib3 {
-  int (*fib)(Class*);
-};
-Vec* Fib3_v = newVec();
-
-int Fib3_Int3_fib(Class* self) {
-  Int3* p = (Int3*)self;
-  if(p-> x < 2) return 1;
-
-  Int3 p1 ={Int3_classId, p->x - 2};
-  Int3 p2 ={Int3_classId, p->x - 1};
-
-  return Fib3_Int3_fib((Class*)&p1) + Fib3_Int3_fib((Class*)&p2);
-}
-
-Fib3* newFib3_Int3() {
-  Fib3 *impl = new Fib3();
-  setVec(Fib3_v, Int3_classId, (void*)impl);
-  impl->fib = &Fib3_Int3_fib;
-  return impl;
-}
-Fib3* Fib3_Int3_ = newFib3_Int3();
-
-
 int main() {
   long start;
 
@@ -130,15 +257,8 @@ int main() {
   printf("%d\n", Fib(40).fib());
   printf("%ld\n", gett() - start);
 
-  start = gett();
-  Int p(40);
-  printf("%d\n", ((Fib2*)Fib2_v->data[p.id])->fib((Class*)&p));
-  printf("%ld\n", gett() - start);
-
-  start = gett();
-  Int3 p3 = {Int3_classId, 40};
-  printf("%d\n", ((Fib2*)Fib3_v->data[p3.id])->fib((Class*)&p3));
-  printf("%ld\n", gett() - start);
+  trait1::bench();
+  trait2::bench();
 
   return 0;
 }
