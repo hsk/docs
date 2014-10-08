75bce1077c484c16f43f8ffd3eaef6b616a8c39b
Hiroshi Sakurai <sakurai777@gmail.com>
2014-10-09 00:17:08 +0900

    added a.rrr

--- pp_ast/test2/main.ml
+++ pp_ast/test2/main.ml
@@ -9,9 +9,13 @@ let compile output input =
   let inp = open_in (input) in
   let lexbuf = Lexing.from_channel inp in
   let ast = Parser.stmts Lexer.token lexbuf in
+Printf.printf "parse ok@.\n"; flush stdout;
+
   let (ast:Stmt.t) = cnv(SList(ast)) in
+Printf.printf "cnv ok@.\n"; flush stdout;
   let cpp = Stmt.print ast in
-  Printf.printf "%s\n" cpp;
+Printf.printf "print ok\n"; flush stdout;
+(*  Printf.printf "%s\n" cpp;*)
   asm_open output;
   asm(cpp);
   asm_close()
