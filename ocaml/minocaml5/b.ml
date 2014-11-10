ef27071c762f9a89491187f463cc0b79b32250d4
Hiroshi Sakurai <sakurai777@gmail.com>
2014-11-10 14:12:38 +0900

    added minocaml5

---
+++ ocaml/minocaml5/b.ml
@@ -0,0 +1,13 @@
+let a = 1
+let add10 b = b + 10
+let _ =
+	Printf.printf "%d\n" (add10 20)
+
+open Printf
+
+let arr = Array.make 1 10
+
+let _ =
+	printf "%d\n" arr.(0);
+	arr.(0) <- 1;
+	printf "%d\n" arr.(0)
