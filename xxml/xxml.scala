95e6f33b04b8f55e12b4107ebe3eff95c4dc287a
Hiroshi Sakurai <sakurai777@gmail.com>
2015-08-04 07:05:55 +0900

    changed xxml.scala

--- xxml/xxml.scala
+++ xxml/xxml.scala
@@ -11,53 +11,62 @@ case class Lst(xs:List[XXML]) extends XXML
 class XXMLParser extends RegexParsers {
   def name = "[!:_a-zA-Z0-9][:_a-zA-Z0-9]*".r
   def text = "[^<]+".r ^^ {Text(_)}
-  def comment = "<!--" ~> rep("[^-]+".r | not("-->") ~> "-") <~ "-->" ^^
+  def comment = "<!--" ~> ("[^-]+".r | not("-->") ~> "-").* <~ "-->" ^^
                 {l=>Comment(l.foldLeft(""){(a,b)=>a+b})}
-  def attr = name ~ opt("=" ~> ("\"([^\"]|\\.)+\"".r ^^ {b=>b.substring(1,b.length-1)} | "[^> ]+".r)) ^^ {
-    case a~Some(b) => (a,b)
-    case a~None => (a,a)
-  } | "\"([^\"]|\\.)+\"".r ^^ {b=>("",b.substring(1,b.length-1))}
 
-  def attrs = rep(attr) ^^ {l => l.foldLeft(Map[String,String]()){case(a,b)=>a + b}}
-  def tagText = ("<" ~> (name ~ "!" ~ attrs) <~ ">") ~ rep("[^<]+".r | not("</") ~> "<") ~
+  def attr = name ~ opt("=" ~> ("\"([^\"]|\\.)+\"".r ^^ {_.substring(1)} | "[^> ]+".r)) ^^ {
+    case a~Some(b) => (a,b)
+    case a~None    => (a,a)
+  } | "\"([^\"]|\\.)+\"".r ^^ {b=>("",b.substring(1))}
+
+  def attrs = (attr).* ^^ {l => l.foldLeft(Map[String,String]()){case(a,b)=>a + b}}
+
+  def tagText = ("<" ~> (name ~ "!" ~ attrs) <~ ">") ~ ("[^<]+".r | not("</") ~> "<").* ~
     opt("<"~>"/"~>opt(name)<~opt(">")) ^^
     { case n~m~a~t~n2=> Block(n+m,a,List(Text(t.foldLeft(""){_+_})))}
+
+  def block = (("<" ~> name ~ attrs <~ ">") ~ elements ~ ("<" ~> "/" ~> opt(name) <~ opt(">"))).filter
+    { case a~at~b~Some(c) => a==c case _ => true } ^^
+    { case a~at~b~c       => Block(a, at, b) }
+
   def one = "<" ~> (name ~ attrs) <~ opt("/") <~ ">" ^^ { case a~b=>One(a,b) }
-  def exp:Parser[XXML] = (comment).
-          | (tagText).
-          | ((("<" ~> name ~ attrs <~ ">") ~ exps ~ ("<" ~> "/" ~> opt(name) <~ opt(">"))).
-            filter {case a~at~b~Some(c) =>a==c case _ => true} ^^
-            { case a~at~b~c => Block(a, at, b) } ).
-          | (one).
-          | (text) 
-  def exps = rep(exp)
-  def parse(input: String) = parseAll(exps, input)
+
+  def element:Parser[XXML] = comment | tagText | block | one | text 
+  def elements = element.*
+  def parse(input: String) = parseAll(elements, input)
 }
-object XXMLParser extends XXMLParser{}
+
+object XXMLParser extends XXMLParser {
+}
 
 object XXMLSchema extends XXMLParser {
-  trait Se
+  sealed trait Se
   case class Rep(a:Se) extends Se
   case class Or(ses:List[Se]) extends Se
   case class Tag(s:String, se:Se) extends Se
   case class Var(s: String) extends Se
   case class Schema(start:String, rules:Map[String, Se])
 
+  def toList(x:XXML):List[XXML] = {
+    x match {
+      case Lst(x) => x
+      case x => List(x)
+    }
+  }
   def tag(name:String, p: =>Parser[XXML]):Parser[XXML] =
-    ("<" ~> (name ).r ~> attrs <~ ">") ~ rep(p) <~ opt("</" <~ opt(name) <~ ">") ^^
-    {case a~b=>Block(name, a, b)} | comment
+    ("<" ~> (name ).r ~> attrs <~ ">") ~ p <~ opt("</" ~ opt(name) ~ ">") ^^
+    { case a~b => Block(name, a, toList(b)) } | comment
 
-  def one2 =
-    not("<![:_a-zA-Z0-9]*".r ~ attrs ~ opt("/") ~ ">") ~> one
+  def schema:Parser[Schema] = rules ^^ { case (n,e)::rules => Schema(n,((n,e)::rules).toMap) case _ => null }
+  def rules = rule.+
 
-  def schema =
-    tag("!RULES",
-      tag("!",
-        tag("!l", text) |
-        tag("!t", one2) |
-        one2
-      )
-    )
+  def rule = (name <~ "=") ~ e ^^ {case name~e => (name, e)}
+  def e:Parser[Se] = repsep(term, "|") ^^ { case List(a)=> a case es => Or(es) }
+  def term = fact <~ "*" ^^ { Rep(_) } | fact
+  def fact = name ^^ { Var(_) } | "(" ~> e <~ ")" | 
+    (("<" ~> name <~ ">") ~ e ~ ("<" ~> "/" ~> opt(name) <~ opt(">"))).filter
+    { case a~b~Some(c) => a==c case _ => true } ^^
+    { case a~b~c       => Tag(a, b) }
 
   def compile(schema:Schema): Parser[XXML] = {
     var env = scala.collection.mutable.Map[String, Parser[XXML]]("text"->text)
@@ -68,8 +77,9 @@ object XXMLSchema extends XXMLParser {
     }
     def comp(se:Se):Parser[XXML] =
       se match {
-        case Rep(se) => rep(comp(se))^^{Lst(_)}
+        case Rep(se) => comp(se).* ^^{Lst(_)}
         case Or(s::ss) => ss.foldLeft(comp(s)){(a,b)=>a|comp(b)}
+        case Or(Nil) => throw new Exception("error")
         case Tag(s,se) => tag(s,comp(se))
         case Var(s) => get(s)
       }
@@ -79,27 +89,7 @@ object XXMLSchema extends XXMLParser {
     env(schema.start)
   }
 
-  def compile(xxml:XXML):Schema = {
-    def schema(xxml:XXML):Schema =
-      xxml match {
-        case Block("!RULES",m,ls) =>
-          Schema(m.toList.head._1, rules(ls))
-      }
-    def rules(ls:List[XXML]):Map[String,Se] =
-      ls.foldLeft(Map[String,Se]()) {
-        case (env,Block("!", m, ls)) =>
-          env + (m.toList.head._1 -> Or(ors(ls)))
-      }
-    def ors(ls:List[XXML]):List[Se] =
-      ls.map {
-        case Block("!l", m, _) => Rep(Var(m.toList.head._1))
-        case Block("!t", m, ls) => Tag(m.toList.head._1, Or(ors(ls)))
-        case One(name,_) => Var(name)
-      }
-    schema(xxml)
-  }
-
-  def parseSchema(input: String) = {
+  def parseSchema(input: String):Schema = {
     val r = parseAll(schema, input)
     r match {
       case Success(res, next) => res
@@ -108,10 +98,9 @@ object XXMLSchema extends XXMLParser {
   }
 
   def compileSchema(input: String) = {
-    val res = parseSchema(input)
+    val res:Schema = parseSchema(input)
     println(res)
-    val res2 = compile(res)
-    compile(res2)
+    compile(res)
   }
 }
 
@@ -130,25 +119,24 @@ object main extends App {
   println("res="+res)
 
   val parser = XXMLSchema.compileSchema("""
-    <!RULES es>
-    <! es><!l e>
-    <! e><tb><text>
-    <! tb><!t table><tr>
-    <! tr><!t tr><td><!t th><td>
-    <! td><!t td><e>
+    start   = exps
+    exps    = exp*
+    exp     = table | div | text
+    div     = <div>exps</div>
+    table   = <table>tr*</table>
+    tr      = <th>td*</th> | <tr>td*</tr>
+    td      = <td>exps</td>
     """)
 
   println(XXMLSchema.parseAll(parser, """
-    <table>
-      <th>
-        <td>aaa
-        <td>aaa
-      <tr>
-        <td>aaa
-        <td>aaa
-    </table>
-    <table>
-      <tr>
-        <td>aaa
+    <div>
+      <table>
+        <tr>
+          <td>aaa
+          <td>aaa
+        <tr>
+          <td>aaa
+          <td>aaa
+    </div>
     """))
 }
